ab6eead4c64851b2568ebf7d00bf14460056aa7d
h1jun <dittoavec@gmail.com>
2021-02-16 17:08:07 +0900

    111
    11111

--- SsangYongBang/src/com/test/sist/TestWonjun.java
+++ SsangYongBang/src/com/test/sist/TestWonjun.java
@@ -2,8 +2,8 @@ package com.test.sist;
 
 public class TestWonjun {
 	public static void main(String[] args) {
-		System.out.println(test);
-		System.out.println(test11);
+		System.out.println("test");
+		System.out.println("test11");
 
 
 }
