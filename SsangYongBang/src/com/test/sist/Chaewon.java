ceca3db9ffd9ce385c4cad62fc33d2515a064c77
imchaewon <76411348+imchaewon@users.noreply.github.com>
2021-02-16 16:17:46 +0900

    Update Chaewon.java

--- SsangYongBang/src/com/test/sist/Chaewon.java
+++ SsangYongBang/src/com/test/sist/Chaewon.java
@@ -4,6 +4,7 @@ public class Chaewon {
 	public static void main(String[] args) {
 		System.out.println(1111);
 		System.out.println(2222);
-		System.out.println(333);
+		System.out.println(3);
+		System.out.println(4);
 	}
 }
