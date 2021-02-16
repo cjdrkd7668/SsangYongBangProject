8fea3d54d2a644db54064f73d0f669cb57eb926f
h1jun <dittoavec@gmail.com>
2021-02-16 16:33:26 +0900

    테스트 중
    test test

--- SsangYongBang/src/com/test/sist/TestWonjun.java
+++ SsangYongBang/src/com/test/sist/TestWonjun.java
@@ -2,12 +2,7 @@ package com.test.sist;
 
 public class TestWonjun {
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		System.out.println("Hello1");
-		System.out.println("Hello2");
-		System.out.println("Hello3");
-		System.out.println("Test11111111");
-		System.out.println("Final Test2!!!!");
+		System.out.println(test);
 	}
 
 }
