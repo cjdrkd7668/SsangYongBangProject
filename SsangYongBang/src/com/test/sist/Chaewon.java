9c68cbf589949d1bad437f042d2982a392d9d82e
imchaewon <76411348+imchaewon@users.noreply.github.com>
2021-02-16 16:51:44 +0900

    Update Chaewon.java

--- SsangYongBang/src/com/test/sist/Chaewon.java
+++ SsangYongBang/src/com/test/sist/Chaewon.java
@@ -2,11 +2,6 @@ package com.test.sist;
 
 public class Chaewon {
 	public static void main(String[] args) {
-		System.out.println(1111);
-		System.out.println(2222);
-		System.out.println(3);
-		System.out.println(4);
-		System.out.println(555555);
-		System.out.println(99999999);
+		System.out.println(111);
 	}
 }
