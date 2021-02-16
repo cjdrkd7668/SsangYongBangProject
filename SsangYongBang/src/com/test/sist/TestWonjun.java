44eb6e087c0fef217cb57efab2a8aa46baf4bf47
h1jun <dittoavec@gmail.com>
2021-02-16 15:29:45 +0900

    테스트2
    테스트2

--- SsangYongBang/src/com/test/sist/TestWonjun.java
+++ SsangYongBang/src/com/test/sist/TestWonjun.java
@@ -3,6 +3,8 @@ package com.test.sist;
 public class TestWonjun {
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		System.out.println("Hello1");
+		System.out.println("Hello2");
 	}
 
 }
