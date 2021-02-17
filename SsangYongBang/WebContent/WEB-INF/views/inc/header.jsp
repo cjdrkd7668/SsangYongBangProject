f01c240a4bbddee2b168f7af0c21e6ff720cc58a
iamleechanmi <leechanmi@kakao.com>
2021-02-17 23:40:59 +0900

    자유게시판 목록 작성
    임시로 커뮤니티 탭 눌렀을 때 나오게 했습니다.

--- SsangYongBang/WebContent/WEB-INF/views/inc/header.jsp
+++ SsangYongBang/WebContent/WEB-INF/views/inc/header.jsp
@@ -14,9 +14,9 @@
 					<li><a href="/sybang/house/search.do">방찾기</a></li>
 					<li><a href="/sybang/hope/list.do">희망입주하기</a></li>
 					<li><a href="/sybang/house/post.do">방내놓기</a></li>
-					<li><a href="#">커뮤니티</a></li>
+					<li><a href="/sybang/board/freelist.do">커뮤니티</a></li>
 					<li><a href="/sybang/broker/brokerMain.do">중개사공간</a></li>
-					<li><a href="#">청소/시공</a></li>
+					<li><a href="/sybang/service/servicemain.do">청소/시공</a></li>
 				</ul>
 					<p class="navbar-text navbar-right"><a href="#" class="navbar-link">회원가입</a></p>
 					<p class="navbar-text navbar-right"><a href="#" class="navbar-link">로그인</a></p>
---
+++ SsangYongBang/src/com/test/sist/board/FreeList.java
@@ -0,0 +1,20 @@
+package com.test.sist.board;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/board/freelist.do")
+public class FreeList extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/board/freelist.jsp");
+		dispatcher.forward(req, resp);
+	}
+}
