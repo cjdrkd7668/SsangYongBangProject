d378b8613204374283d47b59acdb5b962fc1132e
iamleechanmi <leechanmi@kakao.com>
2021-02-25 16:21:34 +0900

    자유게시판(댓글 수정) 구현
    * CSS가 난리여서 전반적으로 다시 디자인했습니다.
    * 중개사와 회원 분리해서 프로시저 작성하기엔 시간이 없어 프로시저 수정이 많습니다.
    * 여태 수정한 내역
    1. 자유게시판 글 작성 수정
    2. 자유게시파 댓글 작성 구현
    3. 자유게시판 댓글 삭제 구현
    
    * 버튼이 보이고 안 보이고는 access 값과 청강님이 만든 zerobonem 값을 비교했습니다.
    * 댓글 수정은 ajax 사용해서 써보려고 하는데 문의게시판 이후에 구현 예정입니다.

--- SsangYongBang/src/com/test/sist/free/CommentOk.java
+++ SsangYongBang/src/com/test/sist/free/CommentOk.java
@@ -1,23 +1,73 @@
 package com.test.sist.free;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.test.sist.free.dao.FreeCommentDAO;
+import com.test.sist.free.dto.FreeCommentDTO;
 
 @WebServlet("/free/commentok.do")
 public class CommentOk extends HttpServlet {
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		//댓글 번호
-		String seq = req.getParameter("seq");
-		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/free/commentok.jsp");
-		dispatcher.forward(req, resp);
+		HttpSession session = req.getSession();
+		
+		req.setCharacterEncoding("UTF-8");
+		
+		//게시글 번호
+		String frseq = req.getParameter("frseq");
+
+		//댓글 내용
+		String detail = req.getParameter("detail");
+		
+		FreeCommentDAO dao = new FreeCommentDAO();
+		FreeCommentDTO dto = new FreeCommentDTO();
+		
+		dto.setFrseq(frseq);
+		dto.setDetail(detail);
+		
+		// 중개사일 경우
+		if ((String) session.getAttribute("seq") == null) {
+			dto.setAuthorseq((String)session.getAttribute("approBrokerSeq"));
+			dto.setZerobonem("0");
+
+		} else {
+			// 회원일 경우
+			dto.setAuthorseq((String)session.getAttribute("seq"));
+			dto.setZerobonem("1");
+		}
+		
+		//댓글 작성
+		int result = dao.post(dto);
+		
+		if (result == 1) {
+			
+			//성공 시 보고 있던 게시글로 이동
+			resp.sendRedirect("/sybang/free/detail.do?seq=" + frseq);
+			
+		} else {
+			
+			//실패 시
+			PrintWriter writer = resp.getWriter();
+
+			writer.print("<html><body>");
+			writer.print("<script>");
+			writer.print("alert('failed');");
+			writer.print("history.back();");
+			writer.print("</script>");
+			writer.print("</body></html>");
+
+			writer.close();
+		}
+
 	}
 }
--- SsangYongBang/src/com/test/sist/free/DeleteCommentOk.java
+++ SsangYongBang/src/com/test/sist/free/DeleteCommentOk.java
@@ -1,36 +1,41 @@
-package com.test.sist.board;
+package com.test.sist.free;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/board/freedeletecommentok.do")
-public class FreeDeleteCommentOk extends HttpServlet {
+import com.test.sist.free.dao.FreeCommentDAO;
+
+@WebServlet("/free/deletecommentok.do")
+public class DeleteCommentOk extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-		String seq = req.getParameter("seq"); // 댓글 번호
-		String frseq = req.getParameter("frseq"); // 글 번호
-
-		FreeCommentDAO dao = new FreeCommentDAO();
-
-		// 댓글 삭제
-		int result = dao.del(seq);
-
-		if (result == 1) {
-
-			// 댓글 삭제 성공 시
-			resp.sendRedirect("/sybang/board/freedetail.do?seq=" + frseq);
 		
+		//삭제하려는 댓글 번호
+		String seq = req.getParameter("seq");
+		
+		//삭제하려는 댓글의 글 번호
+		String frseq = req.getParameter("frseq");
+		
+		FreeCommentDAO dao = new FreeCommentDAO();
+		
+		//댓글 삭제
+		int result = dao.del(seq);
+		
+		if (result == 1) {
+			
+			//삭제 성공 시
+			resp.sendRedirect("/sybang/free/detail.do?seq=" + frseq);
 		} else {
 			
-			//댓글 삭제 실패 시
+			//실패 시
 			PrintWriter writer = resp.getWriter();
 
 			writer.print("<html><body>");
