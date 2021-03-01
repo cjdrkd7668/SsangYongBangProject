c0ac391b62d5d1c068cc1d07dbc596dddc1c2b7a
cjdrkd7668 <cjdrkd7668@gmail.com>
2021-03-02 02:06:01 +0900

    승인 요청 게시판 구현중
    관리자 프로시저 업데이트가 있습니다. 삭제 및 승인 기능까지
    구현 후, 드라이브에 업데이트 하겠습니다.

---
+++ SsangYongBang/src/com/test/sist/approval/ApprovalDAO.java
@@ -0,0 +1,159 @@
+package com.test.sist.approval;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.test.sist.DBUtil;
+
+public class ApprovalDAO {
+	
+	private Connection conn;
+	private Statement stat;
+	private PreparedStatement pstat;
+	private CallableStatement cstat;
+	private ResultSet rs;
+	
+	public ApprovalDAO() {
+		
+		try {
+			
+			conn = DBUtil.open();
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+	}
+	
+	public void close() {
+		
+		try {
+			
+			conn.close();
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+	}
+		
+	//미승인 중개사/서비스업체 목록 반환
+	public ArrayList<ApprovalDTO> list(HashMap<String, String> map) {
+		
+		try {
+			
+			String sql = String.format("select * from (select rownum as rnum, nap.* from vwNonApproval nap) where rnum between %s and %s"
+						, map.get("begin")
+						, map.get("end")
+						);
+			
+			System.out.println(sql);
+			
+			pstat = conn.prepareStatement(sql);
+			rs = pstat.executeQuery();
+			
+			ArrayList<ApprovalDTO> list = new ArrayList<ApprovalDTO>();
+			
+			while (rs.next()) {
+				ApprovalDTO dto = new ApprovalDTO();
+				
+				dto.setCategory(rs.getString("category"));
+				dto.setSeq(rs.getString("seq"));
+				dto.setName(rs.getString("name"));
+				dto.setEmail(rs.getString("email"));
+				dto.setTel(rs.getString("tel"));
+				dto.setAddress(rs.getString("address"));
+				
+				list.add(dto);
+			}
+			
+			return list;
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		return null;
+
+	}
+
+	public int getTotalCount() {
+
+		try {
+			
+			String sql = "select count(*) as cnt from vwNonApproval";
+			
+			pstat = conn.prepareStatement(sql);
+			rs = pstat.executeQuery();
+			
+			if (rs.next()) {
+				return rs.getInt("cnt");
+			}
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		return 0;
+	}
+	
+	//승인 요청 삭제 => 각각의 미승인 계정 탈퇴 시키기
+	public int deleteBroker(String seq) {
+		
+		try {
+			
+			String sql = "update tblBroker set delFlag = 1 where seq = " + seq;
+			
+			pstat = conn.prepareStatement(sql);
+			
+			return pstat.executeUpdate();
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		return 0;
+	}
+	public int deleteFirm(String seq) {
+		
+		try {
+			
+			String sql = "update tblFirm set delFlag = 1 where seq = " + seq;
+			
+			pstat = conn.prepareStatement(sql);
+			
+			return pstat.executeUpdate();
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		return 0;
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
